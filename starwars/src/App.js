ba84e40ef7625d52fa4039f0ff36cb0c4b621eaf
FreedomWriter <natalienetworks@gmail.com>
2019-09-27 22:59:36 -0500

    mvp done!

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import CardList from "./components/CardComponents/CardList";
+import { Button } from "reactstrap";
+import Find from "../src/components/FindComponent/Find";
+import styled from "styled-components";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [showContent, setShowContent] = useState(false);
+
+  const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin: 2%;
+  `;
+
+  function toggleContentHandler() {
+    const doesShow = showContent;
+    setShowContent(!doesShow);
+  }
+
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
+    <div>
+      <Wrapper>
+        <Button onClick={toggleContentHandler}>Explore the Characters</Button>
+        <Find />
+      </Wrapper>
+
+      {showContent ? (
+        <div>
+          <CardList />
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
