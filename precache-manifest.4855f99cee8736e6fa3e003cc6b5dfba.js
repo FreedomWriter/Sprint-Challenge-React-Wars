10dbb1c34764be87a45d70527cabd4a54c159eac
FreedomWriter <natalienetworks@gmail.com>
2019-09-29 17:52:01 -0500

    Updates

---
+++ precache-manifest.4855f99cee8736e6fa3e003cc6b5dfba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462120c11d7e5acb2ec6576070a2eff5",
+    "url": "/Sprint-Challenge-React-Wars/index.html"
+  },
+  {
+    "revision": "d52a07da47dd79430ec6",
+    "url": "/Sprint-Challenge-React-Wars/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d9581df1752d96f89700",
+    "url": "/Sprint-Challenge-React-Wars/static/css/main.0c77d7da.chunk.css"
+  },
+  {
+    "revision": "d52a07da47dd79430ec6",
+    "url": "/Sprint-Challenge-React-Wars/static/js/2.1ce31c85.chunk.js"
+  },
+  {
+    "revision": "d9581df1752d96f89700",
+    "url": "/Sprint-Challenge-React-Wars/static/js/main.3e795728.chunk.js"
+  },
+  {
+    "revision": "79e290dbb9d404c6a381",
+    "url": "/Sprint-Challenge-React-Wars/static/js/runtime~main.e7db6641.js"
+  },
+  {
+    "revision": "dff75a1ffda792b2380226033f28415c",
+    "url": "/Sprint-Challenge-React-Wars/static/media/sw-bg.dff75a1f.jpg"
+  }
+]);
