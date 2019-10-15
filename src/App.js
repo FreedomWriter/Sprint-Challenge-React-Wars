ce0bc99758d3c7d3f7ef663e2ac332a6aae03694
FreedomWriter <natalienetworks@gmail.com>
2019-10-15 14:33:22 -0500

    final

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import HeaderComp from "./components/HeaderComponet/Header";
+import Crawl from "./components/CrawlComponent/Crawl";
+import BodyComp from "./components/BodyComponents/BodyComp";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <HeaderComp />
+      </div>
+      <div>
+        <Crawl />
+      </div>
+      <div>
+        <BodyComp />
+      </div>
+    </div>
+  );
+};
+
+export default App;
