3e52b62d364b138e237589d527a918f8c59db6c0
FreedomWriter <natalienetworks@gmail.com>
2019-10-15 17:55:04 -0500

    Looking like issues might be on the way to resolved

---
+++ starwars/precache-manifest.8023135498c84ee8f16b3cabc4cca620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5beb837ea899d324a57442660671c6a",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "ed9f2deddad4be5053d5",
+    "url": "/starwars/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6f164dfa80e656cf063b",
+    "url": "/starwars/static/css/main.8c8bbb2f.chunk.css"
+  },
+  {
+    "revision": "ed9f2deddad4be5053d5",
+    "url": "/starwars/static/js/2.03dcae9e.chunk.js"
+  },
+  {
+    "revision": "6f164dfa80e656cf063b",
+    "url": "/starwars/static/js/main.d62156d6.chunk.js"
+  },
+  {
+    "revision": "9cd712b42b70d05ab420",
+    "url": "/starwars/static/js/runtime~main.f4b6364c.js"
+  },
+  {
+    "revision": "dff75a1ffda792b2380226033f28415c",
+    "url": "/starwars/static/media/sw-bg.dff75a1f.jpg"
+  }
+]);
