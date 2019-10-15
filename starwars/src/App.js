3846625cf54a8560545e8a4935ab6a7cad497a11
FreedomWriter <natalienetworks@gmail.com>
2019-10-15 17:19:48 -0500

    Revert "pages"
    "fixing mistakes"
    This reverts commit a6c0d1a52fa17fd4d78d576f5274c713beec9028, reversing
    changes made to d8986c46d19a13aa1caea48c2291ef6022268082.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import HeaderComp from "./components/HeaderComponet/Header";
 import Crawl from "./components/CrawlComponent/Crawl";
 import BodyComp from "./components/BodyComponents/BodyComp";
-import Footer from "../src/components/FooterComponent/Footer";
 
 const App = () => {
   return (
@@ -16,9 +15,6 @@ const App = () => {
       </div>
       <div>
         <BodyComp />
-      </div>
-      <div>
-        <Footer />
       </div>
     </div>
   );
